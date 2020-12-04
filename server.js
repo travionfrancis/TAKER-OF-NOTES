79b7b052f258e93702e37b6e320c2c4c14d465e2
TraviFravi <travi1500@gmail.com>
2020-12-04 00:27:20 -0500

    Looking at the Guides and this is how they are telling me how to do this

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-// Pseudocode
 // preemptive file
 // The Heroku Guide should help
+
+// has to be set up as an express server
+
+// dependencies
+const express = require("express");
+const app = express();
+const path = require("path");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
