ecb09465a89efa514a04564063e0ccda669a8357
TraviFravi <travi1500@gmail.com>
2020-12-04 00:33:15 -0500

    Added server port and app.use functions

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ const app = express();
 const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+// SERVER PORT PLEASE REMEMBER
+const PORT = process.env.PORT || 8080;
+
+// express data stuff (its parsing, just in case)
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(_dirname, 'public')));
